3b708eaac0892411bbffea1e18a799a4ade6e655
Rajanjor <81916900+123452123-codr@users.noreply.github.com>
2023-05-02 11:02:27 +0530

    few additions...

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def check_all_messages(message):
 
     # Responses -------------------------------------------------------------------------------------------------------
     response('Hello!', ['hello', 'hi', 'hey', 'sup', 'heyo'], single_response=True)
-    response('See you!', ['bye', 'goodbye'], single_response=True)
+    response('See you!', ['bye', 'goodbye',], single_response=True)
     response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
     response('You\'re welcome!', ['thank', 'thanks'], single_response=True)
     response('Thank you!', ['i', 'love', 'code', 'palace'], required_words=['code', 'palace'])
