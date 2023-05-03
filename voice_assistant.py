0d2b834babcc6ffa0dfa0e5fa3e0e377e8840efb
123452123-codr <81916900+123452123-codr@users.noreply.github.com>
2023-05-03 17:17:01 +0530

    Adding voice feature...

--- voice_assistant.py
+++ voice_assistant.py
@@ -1,4 +1,47 @@
+import datetime
 import pyttsx3
-engine = pyttsx3.init()
-engine.say("Enter a number")
-engine.runAndWait()
+import speech_recognition as sr
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wish():
+    hour = int(datetime.datetime.now().hour)
+    if 0 <= hour < 12:
+        speak("Good morning!")
+    elif 12 <= hour < 18:
+        speak("Good afternoon!")
+    else:
+        speak("Good evening!")
+
+    speak("Hello Chintu, I am your personal assistant. How may I help you?")
+
+
+def command():
+    r = sr.Recognizer()
+    with sr.Microphone as source:
+        print("listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language="en-in")
+        print(f"User said this: {query}\n")
+
+    except Exception as e:
+        print(e)
+
+        print("Say that again..")
+        return "None"
+
+
+if __name__ == '__main__':
+    wish()
